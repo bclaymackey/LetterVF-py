d070867cbc22323bff8c88643f1aa71afda560cc
bclaymackey <93794661+bclaymackey@users.noreply.github.com>
2021-11-17 08:42:34 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='LetterVF',
+      version='0.0.1',
+      description='automatic cluster analysis of phonemic verbal fluency task data',
+      author='Brandon C. Mackey',
+      author_email='bclaymackey@gmail.com',
+      url='https://github.com/Visscher-Lab/LetterVF'
+     )
