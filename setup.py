0cfc71deedcbb9589d16ba1191e37429ca2ce99c
bclaymackey <93794661+bclaymackey@users.noreply.github.com>
2021-11-18 10:25:04 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Brandon Clay Mackey',                   # Type in your name
   author_email = 'bclaymackey@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/bclaymackey/LetterVF-py',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/bclaymackey/LetterVF-py/archive/refs/tags/0.0.1.tar.gz',    # I explain this later on
   keywords = ['VERBAL FLUENCY', 'STRATEGY', 'PHONEMIC VERBAL FLUENCY TASK'],   # Keywords that define your package best
   install_requires=[
           'xlsxwriter',
